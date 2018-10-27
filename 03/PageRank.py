d3f32d98aba7afa430da9d1ffe52b2b22afcb688
Laura <lazeru08@gmail.com>
2018-10-27 20:13:48 +0200

    Cleaning code a little. Still dividing by zero, tho

--- 03/PageRank.py
+++ 03/PageRank.py
@@ -5,11 +5,10 @@ import time
 import sys
 
 class Edge:
-    def __init__ (self, origin=None, dest=None, hashKey=None):
+    def __init__ (self, origin=None, dest=None):
         self.origin = origin
         self.dest = dest
-        self.hashKey = hashKey
-        self.weight = 0 # write appropriate value
+        self.weight = 1
 
     def __repr__(self):
         return "edge: {0} {1} {2}".format(self.origin, self.dest, self.weight)
@@ -21,18 +20,18 @@ class Airport:
         self.routes = []
         self.routeHash = dict()
         self.outweight = 0   # write appropriate value
+        self.pageIndex = 0
 
     def __repr__(self):
         return "{0}\t{2}\t{1}".format(self.code, self.name, self.pageIndex)
 
-edgeList = [] # list of Edge
 edgeHash = dict() # hash of edge to ease the match
 airportList = [] # list of Airport
 airportHash = dict() # hash key IATA code -> Airport
 P = []       # Initial PageRank vector
 
 def readAirports(fd):
-    print "Reading Airport file from {0}".format(fd)
+    print("Reading Airport file from {0}".format(fd))
     airportsTxt = open(fd, "r");
     cont = 0
     for line in airportsTxt.readlines():
@@ -50,59 +49,43 @@ def readAirports(fd):
             airportList.append(a)
             airportHash[a.code] = a
     airportsTxt.close()
-    print "There were {0} Airports with IATA code".format(cont)
+    print("There were {0} Airports with IATA code".format(cont))
 
 
 def readRoutes(fd):
-    print "Reading Routes file from {0}".format(fd)
+    print("Reading Routes file from {0}".format(fd))
     routesTxt = open(fd, "r");
-    cont = 0
-    testCount = 0
+    counter = 0
     for line in routesTxt.readlines():
+        temp = line.split(',')
+        
         e = Edge()
-        testCount = testCount + 1
-        try:
-            temp = line.split(',')
-            if (len(temp[2]) != 3 or len(temp[4]) != 3) :
-                raise Exception('One of the routes does not contain an IATA code')
+        e.origin = temp[2]
+        e.dest = temp[4]
+        # Check we have these airports
+        if((airportHash.get(e.origin) is None) 
+            or (airportHash.get(e.dest) is None)):
+            continue
+
+        airportHash[e.origin].outweight += 1
             
-            e.origin = temp[2]
-            e.dest = temp[4]
+        hashKey = e.origin + e.dest
+        existingEdge = edgeHash.get(hashKey)
+        if(existingEdge is not None): 
+            existingEdge.weight += 1
+        else:
+            edgeHash[hashKey] = e
+            counter += 1
 
-            # Update airport
-            airport = airportHash[e.origin]
-            airport.outweight += 1
+            #Add route to airports
+            airportHash[e.dest].routes.append(e)
 
-            hashKey = e.origin + e.dest
-
-            if(hashKey in edgeHash.keys()): 
-            # If there already exists an edge from orig to dest
-                e = edgeHash[hashKey]
-                e.weight = e.weight + 1
-                
-            else:
-                # Not in edges list!
-                e.hashKey = hashKey
-                e.weight = 1
-
-                # Add it
-                edgeHash[hashKey] = e
-                edgeList.append(e)
-                cont += 1
-
-        except Exception as inst:
-            pass
        
     routesTxt.close()
-    for key, value in edgeHash.iteritems():
-        edgeList.append(value)
-
-    print "There were {0} routes with IATA code".format(cont)
-    #print edgeHash
-    #print edgeList
+    print("There were {0} routes with IATA code".format(counter))
 
 
-def computeQ(P, n, i):
+def computeSumDestVert(P, n, i):
     overallSum = 0
     for j in range(0, n):
         w_j_i = 0
@@ -111,8 +94,7 @@ def computeQ(P, n, i):
 
         edgeCode = airport_j.code + airport_i.code
         if(edgeCode in edgeHash.keys()):
-            e = edgeHash[edgeCode]
-            w_j_i = e.weight
+            w_j_i  = edgeHash[edgeCode].weight
 
         # Else Do nothing, weight is 0.        
         overallSum += P[j] * w_j_i / airport_j.outweight
@@ -127,7 +109,7 @@ def computePageRanks():
     while(it < 10):  # TODO: Change this
         Q = [0] * n
         for i in range(0, n):
-            Q[i] = (L * computeQ(P, n, i)) + ((1 - L)/n)
+            Q[i] = (L * computeSumDestVert(P, n, i)) + ((1 - L)/n)
         P = Q
     
     return it
@@ -137,8 +119,15 @@ def outputPageRanks():
     # write your code
 
 def main(argv=None):
+    time1 = time.time()
     readAirports("airports.txt")
     readRoutes("routes.txt")
+    time2 = time.time()
+    print("Time of reading airports and routes:", time2-time1)
+    print("****************************************")
+    #print(airportHash
+    print("#########################################")
+
 
     time1 = time.time()
     iterations = computePageRanks()
@@ -146,8 +135,8 @@ def main(argv=None):
     
     if(False):
         outputPageRanks()
-        print "#Iterations:", iterations
-        print "Time of computePageRanks():", time2-time1
+        print("#Iterations:", iterations)
+        print("Time of computePageRanks():", time2-time1)
 
 
 if __name__ == "__main__":
