28749c72fd362527bb209017fae972539a2b1fdf
Laura <lazeru08@gmail.com>
2018-11-28 09:21:14 +0100

    Adding 04 + skeleton of 5

---
+++ 05/code.R
@@ -0,0 +1,24 @@
+# Load and install necessary packages
+requiredPackages <- c("igraph", "ggplot2", "Matrix", "ggthemes", "gridExtra")
+
+for (pac in requiredPackages) {
+    if(!require(pac,  character.only=TRUE)){
+        install.packages(pac, repos="http://cran.rstudio.com")
+        library(pac,  character.only=TRUE)
+    } 
+}
+rm(pac)
+rm(requiredPackages)
+
+
+# Set WD and load data
+wd = getwd()
+if(grepl("nora", wd)) {
+    setwd("~/Documents/18-19/IR/LABS/05/")
+} else {
+    # Set Carolina Working directory
+    setwd("")
+}
+rm(wd)
+
+#################
