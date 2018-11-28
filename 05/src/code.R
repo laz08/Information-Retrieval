f5c4429f01a0f80ae2431b6de79a1b3c93c6dcb1
Laura <lazeru08@gmail.com>
2018-11-28 16:31:24 +0100

    First selection of Laura's songs

--- 05/src/code.R
+++ 05/src/code.R
@@ -26,30 +26,51 @@ songdata <- read.csv("./datasets/songdata.csv", encoding="851", stringsAsFactors
 songdata$link <- NULL
 songdata$artist <- as.factor(songdata$artist)
 
-levels(songdata$artist)
+View(levels(songdata$artist))
 
 coldplay <- songdata[songdata$artist == 'Coldplay', ]
-nrow(coldplay)
-
 ABBA <- songdata[songdata$artist == 'ABBA', ]
-nrow(ABBA)
-
 Enya <- songdata[songdata$artist == 'Enya', ]
-nrow(Enya)
-
 Europe <- songdata[songdata$artist == 'Europe', ]
-nrow(Europe)
-
 Queen <- songdata[songdata$artist == 'Queen', ]
-nrow(Queen)
-
 Neil.Young <- songdata[songdata$artist == 'Neil Young', ]
-nrow(Neil.Young)
-
 Pet.Shop.Boys <- songdata[songdata$artist == 'Pet Shop Boys', ]
-nrow(Pet.Shop.Boys)
+
 
 Pink.Floyd <- songdata[songdata$artist == 'Pink Floyd', ]
 nrow(Pink.Floyd)
 
 
+
+### Laura Selection
+
+# View(coldplay[, 1:2])
+coldplaySongs = c("3258", "3275", "3290", "3292", "3301", "3321", "3331", "28155", "28169", "28171", "28184", "28188")
+coldplayLaura <- coldplay[coldplaySongs, ]
+
+#View(ABBA[, 1:2])
+abbaSongs = c("8", "19", "23", "46", "57", "63", "107", "108")
+abba.Laura <- ABBA[abbaSongs, ]
+
+enyaSongs = c("5266", "31691", "5256")
+enya.Laura <- Enya[enyaSongs, ]
+
+europeSongs <- c("31913", "31926", "31959")
+europe.Laura <- Europe[europeSongs, ]
+
+queenSongs <- c("16529", "16545", "16562", "16576", "49281", "49283", "49285", "49299", "49321", "49322", "49377")
+queen.Laura <- Queen[queenSongs, ]
+
+neilSongs <- c("13880")
+neil.Laura <- Neil.Young[neilSongs, ]
+
+psb <- c("15798", "15800", "15802", "15804", "15811", "15814", "15819", "15821", "15837", "15844", "15858", "47958", "47966", "47968", "47970", "47979", "47986", "47996", "48020", "48021")
+psb.Laura <- Pet.Shop.Boys[psb, ]
+
+laura.selection = rbind(coldplayLaura, abba.Laura)
+laura.selection = rbind(laura.selection, enya.Laura)
+laura.selection = rbind(laura.selection, europe.Laura)
+laura.selection = rbind(laura.selection, queen.Laura)
+laura.selection = rbind(laura.selection, neil.Laura)
+laura.selection = rbind(laura.selection, psb.Laura)
+nrow(laura.selection)
